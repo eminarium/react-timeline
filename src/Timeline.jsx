19cb5717e90ece756b9d1318df13223bd669f0de
Merdan D <merdan.durdiyev@gmail.com>
2023-12-26 02:36:56 +0500

    Adds a way out to recognize colors

--- src/Timeline.jsx
+++ src/Timeline.jsx
@@ -5,6 +5,14 @@ export default function Timeline({ defaultColor }) {
   return (
     <div>
       {timelineElements.map((element) => {
+        const colors = [
+          "bg-red-500",
+          "bg-blue-500",
+          "bg-yellow-500",
+          "bg-purple-500",
+          "bg-orange-500",
+        ];
+
         const color = defaultColor || `bg-${element.color}-500`;
 
         return (
